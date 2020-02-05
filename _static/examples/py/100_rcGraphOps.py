81a97948580ded4111f7e4b3576b08381fb846ea
Jakob Lykke Andersen <Jakob@caput.dk>
2020-02-05 14:04:22 +0100

    v0.10 docs

---
+++ _static/examples/py/100_rcGraphOps.py
@@ -0,0 +1,26 @@
+include("050_formoseGrammar.py")
+glycolaldehyde.print()
+# A graph G can be used to construct special rules:
+# (\emptyset <- \emptyset -> G)
+bindExp = rcBind(glycolaldehyde)
+# (G <- \emptyset -> \emptyset)
+unbindExp = rcUnbind(glycolaldehyde)
+# (G <- G -> G)
+idExp = rcId(glycolaldehyde)
+# These are really rule composition expressions that have to be evaluated:
+rc = rcEvaluator(inputRules)
+# Each expression results in a lists of rules:
+bindRules = rc.eval(bindExp)
+unbindRules = rc.eval(unbindExp)
+idRules = rc.eval(idExp)
+postSection("Bind Rules")
+for p in bindRules:
+	p.print()
+postSection("Unbind Rules")
+for p in unbindRules:
+	p.print()
+postSection("Id Rules")
+for p in idRules:
+	p.print()
+# rst-name: Unary Operators
+# rst: Special rules can be constructed from graphs.
---
+++ _static/examples/pymod_extension/stuff.hpp
@@ -0,0 +1,12 @@
+#ifndef AWESOMELIB_HPP
+#define AWESOMELIB_HPP
+
+#include <mod/graph/Graph.hpp>
+
+namespace awesome {
+
+std::shared_ptr<mod::graph::Graph> doStuff();
+
+} // namespace awesome
+
+#endif // AWESOMELIB_HPP
