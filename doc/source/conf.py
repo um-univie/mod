6f941b7fcbf62f72a017306bfe0e59e5568a27a3
Jakob Lykke Andersen <Jakob@caput.dk>
2021-07-08 11:32:08 +0200

    Update copyright year

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -161,7 +161,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MØD'
-copyright = u'2013-2020, Jakob Lykke Andersen'
+copyright = u'2013-2021, Jakob Lykke Andersen'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
